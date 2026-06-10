73f5c2d399ff07eb64daae48a2a026baa5d12f9a
moniquemedeiros-design <monique.medeiros@ba.estudante.senai.br>
2026-06-10 15:12:32 -0300

    commit 3

--- desafio7/src/app/app.routes.ts
+++ desafio7/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Login },
+    { path: '', component: Home },
+    { path: '', component: Dashboard }
+];
--- desafio7/src/app/pages/home/home.ts
+++ desafio7/src/app/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { getXHRResponse } from 'rxjs/internal/ajax/getXHRResponse';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
